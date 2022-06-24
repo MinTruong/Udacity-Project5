95113c517b7c714a5be77e479da1201d744136a8
MinhTruong <minhcm2@gmail.com>
2022-06-24 12:16:10 +0700

    gant permision

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,11 +2,11 @@
 
 # Step 1
 # Run the Docker Hub container with kubernetes
-kubectl apply -f deployment_project.yaml
-kubectl expose deployment project-4 --port=80 --name=service-project-4
+kubectl apply -f deployment_project5.yaml
+kubectl expose deployment project-5 --port=80 --name=service-project-5
 # Step 2:
 # List kubernetes pods
 kubectl get pods
 # Step 3:
 # Forward the container port to a host
-kubectl expose deployment project-4 --type=LoadBalancer --port=9000 --target-port=80
+kubectl expose deployment project-5 --type=LoadBalancer --port=9000 --target-port=80
